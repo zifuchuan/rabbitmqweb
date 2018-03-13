84581dd47e866e1ac3d06e8c34fd0c3b06750f0e
Zhongyuan Wang <wzhy989@163.com>
2018-03-13 15:56:03 +0800

    2018/03/13/15/55

--- src/main/java/com.wzhy.mq/config/ApplicationConfig.java
+++ src/main/java/com.wzhy.mq/config/ApplicationConfig.java
@@ -3,5 +3,6 @@ package com.wzhy.mq.config;
 public class ApplicationConfig {
     public static void main(String[] args) {
         System.out.println("testsd");
+        System.out.println();
     }
 }
